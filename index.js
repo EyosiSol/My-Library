92623269ba3fd7d2283aabacdcc8be75b067cca0
eyosHP250 <eyosila123@gmail.com>
2024-04-27 21:15:59 +0300

    add a modal/dialog div as a title and author submit

---
+++ index.js
@@ -0,0 +1,7 @@
+const openModal = document.querySelector(".add")
+const submit = document.querySelector("#submit")
+const modal = document.querySelector("#modal")
+
+openModal.addEventListener('click',()=>{
+    modal.showModal()
+})
