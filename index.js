50b876e47b1bbd069d9cc56fb97b4878e938b3d8
eyosHP250 <eyosila123@gmail.com>
2024-04-28 19:41:49 +0300

    add the basic functionality to append on the page while filling the form

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
-const openModal = document.querySelector(".add")
-const submit = document.querySelector("#submit")
-const modal = document.querySelector("#modal")
+const openModal = document.querySelector(".add");
+const submit = document.querySelector("#submit");
+const modal = document.querySelector("#modal");
 
-openModal.addEventListener('click',()=>{
-    modal.showModal()
-})
+const form = document.querySelector("#book-form");
+const booksGrid = document.querySelector(".grid-container");
+
+openModal.addEventListener("click", () => {
+  modal.showModal();
+});
+
+form.addEventListener("submit", () => {
+  const title = document.getElementById("title").value;
+  const author = document.getElementById("author").value;
+  const page = document.getElementById("pages").value;
+
+  const newBookCard = document.createElement("div");
+
+  newBookCard.classList.add(".grid-container>div");
+  newBookCard.innerHTML = `
+    <div class="temp1">
+        <div class="title">${title}</div> 
+        <div class="author">${author}</div>
+        <div class="page">${page}</div>
+
+        <div class="read-remove">
+        <button class="read">Have Read</button>
+        <button class="remove">Remove</button>
+    </div>
+    </div>
+    `;
+
+  booksGrid.appendChild(newBookCard);
+  form.reset();
+
+  const modal = document.querySelector("#modal");
+  modal.close();
+});
